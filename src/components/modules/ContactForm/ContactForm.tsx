0353f2d02d3a4bec1552d0495efb83441021dde5
Oleg <ikslprint@gmail.com>
2024-04-30 18:12:05 +0300

    add tablet styles contacts

--- src/components/modules/ContactForm/ContactForm.tsx
+++ src/components/modules/ContactForm/ContactForm.tsx
@@ -39,35 +39,44 @@ export default function ContactForm({}: ContactFormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-      <InputField
-        label="Full Name"
-        name="fullname"
-        placeholder="John Smith"
-        register={register}
-        error={errors?.fullname}
-      />
-      <InputField
-        label="E-mail"
-        name="email"
-        placeholder="johnsmith@email.com"
-        register={register}
-        error={errors?.email}
-      />
-      <label className="'block mb-4 font-extralight text-xs tracking-[0.2em]">
-        <span className="inline-block mb-1">Message</span>
-        <textarea
-          className="text-[13px]  bg-white bg-opacity-5 px-2 py-1.5 placeholder:text-white  placeholder:opacity-20  w-full h-[196px] focus:outline-none focus:bg-opacity-10 resize-none"
-          {...register('message', {})}
+    <form
+      className="md:flex md:items-baseline md:gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+    >
+      <div className="md:w-[221px]">
+        <InputField
+          label="Full Name"
+          name="fullname"
+          placeholder="John Smith"
+          register={register}
+          error={errors?.fullname}
         />
-      </label>
+        <InputField
+          label="E-mail"
+          name="email"
+          placeholder="johnsmith@email.com"
+          register={register}
+          error={errors?.email}
+        />
+      </div>
 
-      <button
-        type="submit"
-        className="block font-medium text-3xl ml-auto uppercase"
-      >
-        Send
-      </button>
+      <div className="flex-grow">
+        <label className="'block mb-4 font-extralight text-xs tracking-[0.2em]">
+          <span className="inline-block mb-1">Message</span>
+          <textarea
+            className="text-[13px]  bg-white bg-opacity-5 px-2 py-1.5 placeholder:text-white  placeholder:opacity-20  w-full h-[196px] focus:outline-none focus:bg-opacity-10 resize-none md:h-[221px]"
+            {...register('message', {})}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="block font-medium text-3xl ml-auto uppercase"
+        >
+          Send
+        </button>
+      </div>
     </form>
   );
 }
