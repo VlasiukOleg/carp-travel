f287a9c5ca5587b5130b7704d9beb9087a6b271d
Oleg <ikslprint@gmail.com>
2024-04-30 10:57:06 +0300

    add tablet styles for section career

--- src/components/modules/CareerForm/CareerForm.tsx
+++ src/components/modules/CareerForm/CareerForm.tsx
@@ -45,65 +45,80 @@ export default function CareerForm(props: CareerFormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-      <InputField
-        label="Full Name"
-        name="fullname"
-        placeholder="John Smith"
-        register={register}
-        error={errors?.fullname}
-      />
-      <InputField
-        label="E-mail"
-        name="email"
-        placeholder="johnsmith@email.com"
-        register={register}
-        error={errors?.email}
-      />
-      <InputField
-        label="Position"
-        name="position"
-        placeholder="Position"
-        register={register}
-        error={errors?.position}
-      />
-      <InputField
-        label="Phone"
-        name="phone"
-        placeholder="+ 38 (097) 12 34 567"
-        register={register}
-        error={errors?.phone}
-      />
-      <label className="'block mb-4 font-extralight text-xs tracking-[0.2em]">
-        <span className="inline-block mb-1">Message</span>
-        <textarea
-          className="text-[13px]  bg-white bg-opacity-5 px-2 py-1.5 placeholder:text-white  placeholder:opacity-20  w-full h-[196px] focus:outline-none focus:bg-opacity-10 resize-none"
-          {...register('message', {})}
+    <form
+      className="md:flex md:gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+    >
+      <div className="md:w-[221px]">
+        <InputField
+          label="Full Name"
+          name="fullname"
+          placeholder="John Smith"
+          register={register}
+          error={errors?.fullname}
         />
-      </label>
-
-      <label className="flex gap-2 mt-4 mb-4 font-extralight text-xs">
-        <input
-          type="checkbox"
-          className="hidden peer/draft"
-          {...register('confirm', {})}
+        <InputField
+          label="E-mail"
+          name="email"
+          placeholder="johnsmith@email.com"
+          register={register}
+          error={errors?.email}
         />
-        <div className="flex items-center justify-center w-[24px] h-[22px] bg-black border-[1px] border-white peer-checked/draft:hidden"></div>
-        <div className="items-center justify-center w-[24px] h-[22px] bg-black border-[1px] border-white hidden peer-checked/draft:flex">
-          <div className=" top-1 left-1 w-3.5 h-3.5 bg-white"></div>
-        </div>
+        <InputField
+          label="Position"
+          name="position"
+          placeholder="Position"
+          register={register}
+          error={errors?.position}
+        />
+        <InputField
+          label="Phone"
+          name="phone"
+          placeholder="+ 38 (097) 12 34 567"
+          register={register}
+          error={errors?.phone}
+        />
+        <label className="'block mb-4 font-extralight text-xs tracking-[0.2em] md:hidden">
+          <span className="inline-block mb-1">Message</span>
+          <textarea
+            className="text-[13px]  bg-white bg-opacity-5 px-2 py-1.5 placeholder:text-white  placeholder:opacity-20  w-full h-[196px] focus:outline-none focus:bg-opacity-10 resize-none"
+            {...register('message', {})}
+          />
+        </label>
 
-        <span className="inline-block">
-          I confirm my consent to the processing of personal data.
-        </span>
-      </label>
+        <label className="flex gap-2 mt-4 mb-4 font-extralight text-xs">
+          <input
+            type="checkbox"
+            className="hidden peer/draft"
+            {...register('confirm', {})}
+          />
+          <div className="flex items-center justify-center w-[24px] h-[22px] bg-black border-[1px] border-white peer-checked/draft:hidden md:w-[34px]"></div>
+          <div className="items-center justify-center w-[24px] h-[22px] bg-black border-[1px] border-white hidden peer-checked/draft:flex">
+            <div className=" top-1 left-1 w-3.5 h-3.5 bg-white"></div>
+          </div>
 
-      <button
-        type="submit"
-        className="block font-medium text-3xl ml-auto uppercase"
-      >
-        Send
-      </button>
+          <span className="inline-block md:leading-5">
+            I confirm my consent to the processing of personal data.
+          </span>
+        </label>
+      </div>
+
+      <div>
+        <label className="'block mb-4 font-extralight text-xs tracking-[0.2em] hidden md:block">
+          <span className="inline-block mb-1">Message</span>
+          <textarea
+            className="text-[13px]  bg-white bg-opacity-5 px-2 py-1.5 placeholder:text-white  placeholder:opacity-20  w-full h-[196px] focus:outline-none focus:bg-opacity-10 resize-none"
+            {...register('message', {})}
+          />
+        </label>
+        <button
+          type="submit"
+          className="block font-medium text-3xl ml-auto uppercase"
+        >
+          Send
+        </button>
+      </div>
     </form>
   );
 }
--- src/sections/Career/Career.tsx
+++ src/sections/Career/Career.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import clsx from 'clsx';
+
 import Section from '@/components/UiKit/Section/Section';
 import Container from '@/components/Container/Container';
 import SectionTitle from '@/components/UiKit/SectionTitle/SectionTitle';
@@ -19,28 +21,43 @@ export default function Career({}: CareerProps) {
       <div className={styles.career}>
         <Section>
           <Container>
-            <SectionTitle>
-              <span className="font-thin">{career.title}</span>{' '}
-              {career.highlightedTitle}
-            </SectionTitle>
-            <div className="flex justify-end mt-6 mb-9">
-              <Text addStyle="max-w-[179px]">{career.description}</Text>
+            <div className="md:flex justify-between">
+              <SectionTitle>
+                <span className="font-thin">{career.title}</span>{' '}
+                {career.highlightedTitle}
+              </SectionTitle>
+
+              <Text addStyle="max-w-[179px]  mt-6 mb-9 ml-auto md:ml-0 md:max-w-[221px] md:my-0 md:text-[13px]">
+                {career.description}
+              </Text>
             </div>
-            <h3 className="font-extralight text-3xl uppercase text-right pr-9">
+            <h3 className="font-extralight text-3xl uppercase text-right  md:w-[221px]">
               {career.subtitle}
             </h3>
-            <ul className="flex flex-col gap-4 mt-9 max-w-[181px]">
-              {benefitsList.map(({ title, description }) => (
-                <li key={title} className="text-end">
-                  <p className="text-sm mb-2">{title}</p>
-                  <p className="text-xs font-extralight">{description}</p>
-                </li>
-              ))}
-            </ul>
+            <div className="md:flex md:gap-5  md:mt-14">
+              <div>
+                <ul className="flex flex-col gap-4 mt-9 max-w-[181px] md:max-w-[221px] md:gap-6 md:mt-0">
+                  {benefitsList.map(({ title, description }) => (
+                    <li key={title} className="text-end">
+                      <p className="text-sm mb-2 md:text-base">{title}</p>
+                      <p className="text-xs font-extralight">{description}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="hidden md:block">
+                <Text addStyle="max-w-[179px] md:text-[13px] md:max-w-[221px] md:mb-8">
+                  {career.formTitle}
+                </Text>
+                <CareerForm />
+              </div>
+            </div>
           </Container>
         </Section>
       </div>
-      <div className={styles.career}>
+
+      <div className={clsx(styles.career, 'md:hidden')}>
         <Section>
           <Container>
             <div className="flex justify-end  mb-9">
