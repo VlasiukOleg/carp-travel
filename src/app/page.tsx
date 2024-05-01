e9a96317f06768b7bd6b9f93f6ff71a921ccd404
Oleg <ikslprint@gmail.com>
2024-05-01 09:50:43 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import Header from '@/components/Header/Header';
-import Hero from '@/sections/Hero/Hero';
+// import Header from '@/components/Header/Header';
+// import Hero from '@/sections/Hero/Hero';
 import About from '@/sections/About/About';
-import OfferSwiper from '@/components/Modules/OfferSwiper/OfferSwiper';
-import Career from '@/sections/Career/Career';
-import Gallery from '@/sections/Gallery/Gallery';
-import Contacts from '@/sections/Contacts/Contacts';
+// import OfferSwiper from '@/components/Modules/OfferSwiper/OfferSwiper';
+// import Career from '@/sections/Career/Career';
+// import Gallery from '@/sections/Gallery/Gallery';
+// import Contacts from '@/sections/Contacts/Contacts';
 
 export default function Home() {
   return (
     <main>
-      <div className="relative">
+      {/* <div className="relative">
         <Header />
         <Hero />
-      </div>
+      </div> */}
       <About />
-      <OfferSwiper />
+      {/* <OfferSwiper />
       <Career />
       <Gallery />
-      <Contacts />
+      <Contacts /> */}
     </main>
   );
 }
