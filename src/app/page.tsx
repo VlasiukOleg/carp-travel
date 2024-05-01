319852c026cb7a9e9b1132ae1f6ef675098f2d69
Oleg <ikslprint@gmail.com>
2024-05-01 12:46:01 +0300

    remove hero from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Header from '@/components/Header/Header';
-import Hero from '@/sections/Hero/Hero';
 import About from '@/sections/About/About';
 import OfferSwiper from '@/components/Modules/OfferSwiper/OfferSwiper';
 import Career from '@/sections/Career/Career';
@@ -13,7 +12,6 @@ export default function Home() {
     <main>
       <div className="relative">
         <Header />
-        <Hero />
       </div>
       <About />
       <OfferSwiper />
