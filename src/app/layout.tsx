bfcf8690387e2b69835b13da05a6169490a411a0
Oleg <ikslprint@gmail.com>
2024-05-03 09:56:46 +0300

    fix opg image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import ogImage from './opengraph-image.jpg';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -10,6 +12,25 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Carp Travel',
   description: 'Uncover CarpathiansSecrets',
+  metadataBase: new URL('https://carptravel-kappa.vercel.app/'),
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
