aee51273547e4af9f462260f3a11a63343795670
Oleg <ikslprint@gmail.com>
2024-05-06 09:44:27 +0300

    fix mobile menu

--- src/components/Modules/MobileMenu/MobileMenu.tsx
+++ src/components/Modules/MobileMenu/MobileMenu.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import clsx from 'clsx';
 
 import MenuBtn from '@/components/UiKit/MenuBtn/MenuBtn';
 import NavList from '@/components/UiKit/NavList/NavList';
+import Container from '@/components/Container/Container';
 
 export interface MobileMenuProps {
   onToggleMobileMenu: () => void;
@@ -17,11 +18,11 @@ export default function MobileMenu({
   return (
     <div
       className={clsx(
-        'fixed z-50 top-0 left-0 w-screen h-screen backdrop-filter backdrop-blur-md bg-opacity-75 bg-gray-800  overflow-hidden flex justify-center items-center transition-opacity',
+        'px-5 py-5 fixed z-50 top-0 left-0 w-screen h-screen backdrop-filter backdrop-blur-md bg-opacity-75 bg-gray-800  overflow-hidden flex justify-center items-center transition-opacity',
         mobileMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
       )}
     >
-      <header className="absolute top-11 right-5">
+      <header className="absolute right-5 top-10">
         <MenuBtn onClick={onToggleMobileMenu}>Close</MenuBtn>
       </header>
       <NavList onClick={onToggleMobileMenu} />
